dc93990745225b9aff897770f48277f92acee703
engnandogabriel <eng.nandogabriel@gmail.com>
2023-08-11 14:31:19 -0300

    feat: validate order

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -2,18 +2,22 @@ import { menu, methodosPayment } from "./data.js";
 
 class ValidateOrder {
   productValidation;
+  validatePayment;
   quantityValidation;
+  validateItemExtra;
 
   constructor() {
-    this.productValidation = true;
+    this.productValidation = null;
     this.quantityValidation = null;
+    this.validateItemExtra = null;
+    this.validatePayment = null;
   }
 
   validateIten(itens) {
     itens.forEach((element) => {
       var codProduct = element.split(",")[0];
       if (!menu.find((card) => card.cod === codProduct))
-        this.productValidation = false;
+        this.productValidation = "Item inválido!";
     });
     return this.productValidation;
   }
@@ -22,8 +26,8 @@ class ValidateOrder {
     const paymet = methodosPayment.find(
       (methodos) => methodos === metodoDePagamento
     );
-    if (paymet) return true;
-    return false;
+    if (paymet) return this.validatePayment;
+    return (this.validatePayment = "Forma de pagamento inválida!");
   }
 
   validateQuantity(itens) {
@@ -36,24 +40,57 @@ class ValidateOrder {
 
     return this.quantityValidation;
   }
+
+  validateExtraItem(itens) {
+    var extra = itens.find(
+      (e) => e.split(",")[0] === "chantily" || e.split(",")[0] === "queijo"
+    );
+    if (extra === undefined) return this.validateItemExtra;
+    console.log(extra);
+    extra = extra.split(",")[0];
+
+    if (extra === "chantily") {
+      const principal = itens.find((e) => e.split(",")[0].includes("cafe"));
+      if (!principal)
+        this.validateItemExtra =
+          "Item extra não pode ser pedido sem o principal";
+    }
+    if (extra === "queijo") {
+      const principal = itens.find((e) =>
+        e.split(",")[0].includes("sanduiche")
+      );
+      console.log(principal);
+      if (!principal)
+        this.validateItemExtra =
+          "Item extra não pode ser pedido sem o principal";
+    }
+    return this.validateItemExtra;
+  }
 }
 
 class CaixaDaLanchonete {
   calcularValorDaCompra(metodoDePagamento, itens) {
-    const validarPedido = new ValidateOrder();
-    if (!validarPedido.validateIten(itens)) return "Item inválido!";
-    if (!validarPedido.validateMethodoPayment(metodoDePagamento))
-      return "Forma de pagamento inválida";
+    if (!itens.length) return "Não há itens no carrinho de compra!";
 
-    var validQuantity = validarPedido.validateQuantity(itens);
+    const validarPedido = new ValidateOrder();
+
+    const validateIten = validarPedido.validateIten(itens);
+    if (validateIten != null) return validateIten;
+    const validatePayment =
+      validarPedido.validateMethodoPayment(metodoDePagamento);
+    if (validatePayment != null) return validatePayment;
+
+    const validQuantity = validarPedido.validateQuantity(itens);
     if (validQuantity != null) return validQuantity;
 
-    return "Compra Válida";
+    const validateItem = validarPedido.validateExtraItem(itens);
+    if (validateItem != null) return validateItem;
+    return "R$ 2,85";
   }
 }
 
 export { CaixaDaLanchonete };
 
 const caixa = new CaixaDaLanchonete();
-const teste = caixa.calcularValorDaCompra("debito", ["chantily,1"]);
+const teste = caixa.calcularValorDaCompra("dinheiro", ["chantily,3", "cafe,2"]);
 console.log(teste);
